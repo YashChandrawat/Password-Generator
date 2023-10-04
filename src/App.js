1a1c81b5df37e6457e3255c6e36cb05f0feacdb6
Yash Chandrawat <yashchandrawat01@gmail.com>
2023-10-04 23:20:32 +0530

    All-things-done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charactersAllowed, setCharactersAllowed] = useState(false);
+  const [password, setPassword] = useState("Password...");
+
+  const passwordGenerate = () => {
+    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (numberAllowed) {
+      str += "0123456789";
+    }
+    if (charactersAllowed) {
+      str += "~`!@#$%^&*(){}[]?/";
+    }
+
+    let newPassword = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * str.length);
+      newPassword += str.charAt(randomIndex);
+    }
+
+    setPassword(newPassword);
+  };
+
+  const handleChangeLength = (e) => {
+    setLength(e.target.value);
+  };
+
+  const handleNumber = (e) => {
+    setNumberAllowed(!numberAllowed);
+  };
+
+  const handleCharacter = (e) => {
+    setCharactersAllowed(!charactersAllowed);
+  };
+
+  const copyToClipboard = () => {
+    const textField = document.createElement("textarea");
+    textField.innerText = password;
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand("copy");
+    textField.remove();
+    alert("Copied To Clipboard");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App text-black">
+      <div className="section">
+        <h1 className="text-4xl text-black font-medium ">Password Generator</h1>
+        <div className="input-group mb-4">
+          <input
+            type="text"
+            value={password}
+            readOnly
+            className="input-field"
+          />
+          <button
+            onClick={copyToClipboard}
+            className="copy-button"
+          >
+            Copy
+          </button>
+        </div>
+        <div className="input-group mb-4">
+          <input
+            type="number"
+            placeholder="Length..."
+            className="input-field"
+            onChange={(e) => setLength(e.target.value)}
+          />
+          <div className="label-checkbox">
+            <label className="ml-2 text-black">Include Numbers:</label>
+            <input
+              type="checkbox"
+              onClick={handleNumber}
+              className="checkbox"
+            />
+          </div>
+          <div className="label-checkbox">
+            <label className="ml-2 text-black">Include Symbols:</label>
+            <input
+              type="checkbox"
+              onClick={handleCharacter}
+              className="checkbox"
+            />
+          </div>
+        </div>
+        <button onClick={passwordGenerate} className="generate-button">
+          Generate Password
+        </button>
+      </div>
     </div>
   );
 }
